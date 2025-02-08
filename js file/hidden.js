713c09ef92789a1ca7878841465bb952d6a09173
Mdarman899 <155474863+Mdarman899@users.noreply.github.com>
2025-02-08 17:18:05 +0600

    delete some coment code of js

--- js file/hidden.js
+++ js file/hidden.js
@@ -3,19 +3,13 @@ function play() {
     // showscreen("second-page")
 }
 
-// const events = document.querySelector('.a');
-
-// events.addEventListener('click', function(){
-//     this.style.backgroundColor = 'red';
-    
-// })
 
 const len = document.querySelectorAll('.seat-number').length;
-console.log(len);
+// console.log(len);
 
 for(var i = 0 ; i < len ; i++) {
    document.querySelectorAll('.seat-number')[i].addEventListener('click' , function(){
-    this.style.backgroundColor = 'black';
+    this.style.backgroundColor = 'green';
     this.style.color = 'white';
     const texts = this.innerText ;
     console.log(texts);
